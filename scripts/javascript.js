044341783ef42c0b54f3ef0e215dc354cc9e184f
Byron <byronwmit@gmail.com>
2022-10-31 17:08:50 +0000

    Basic gameplay implemented

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,43 +1,63 @@
-let playerChoice = 0;
-
 function getComputerChoice(){
-    let choice = (Math.random() * (4 - 1) + 1);
-    choice = Math.trunc(choice);
-    console.log(choice);
+    let computerChoice = (Math.random() * (4 - 1) + 1);
+    computerChoice = Math.trunc(computerChoice);
 
-
-    if (choice == 1){
-        choiceConverted = "rock";    
+    if (computerChoice == 1){
+        choiceConverted = 'Rock';    
     }
-    else if (choice == 2){
-        choiceConverted = "paper";    
+    else if (computerChoice == 2){
+        choiceConverted = 'Paper';    
     }
-    else if (choice == 3){
-        choiceConverted = "scissors";    
+    else if (computerChoice == 3){
+        choiceConverted = 'Scissors';
     }
     else{
-        console.log("Error!")
+        console.log('Error!');
     }
-    console.log(choiceConverted);
+    console.log('Computer Choice: ' + choiceConverted);
     return(choiceConverted);
 }
 
-function setPlayerChoiceRock(){
-    playerChoice="Rock";
-    console.log(playerChoice);
+
+function setPlayerChoice(setPlayerChoice){
+    playerChoice=setPlayerChoice;
+    console.log('Player Choice:' + playerChoice);
     return playerChoice;
 }
 
-function setPlayerChoicePaper(){
-    playerChoice="Paper";
-    console.log(playerChoice);
-    return playerChoice;
-}
-
-function setPlayerChoiceScissors(){
-    playerChoice="Scissors";
-    console.log(playerChoice);
-    return playerChoice;
+function playGame(playerChoice){
+    setPlayerChoice(playerChoice)  
+    computerChoice = getComputerChoice();
+    //Player Chooses Rock
+    if (playerChoice == 'Rock' && computerChoice == 'Rock'){
+        console.log('Tie');
+    }
+    else if (playerChoice == 'Rock' && computerChoice == 'Paper'){
+        console.log('Computer Wins')        ;
+    }
+    else if (playerChoice == 'Rock' && computerChoice == 'Scissors'){
+        console.log('Player Wins');
+    }
+    //Player Chooses Paper
+    else if (playerChoice == 'Paper' && computerChoice == 'Rock'){
+        console.log('Player Wins');
+    }
+    else if (playerChoice == 'Paper' && computerChoice == 'Paper'){
+        console.log('Tie')        ;
+    }
+    else if (playerChoice == 'Paper' && computerChoice == 'Scissors'){
+        console.log('Computer Wins');
+    }
+    //Player Chooses Scissors
+    else if (playerChoice == 'Scissors' && computerChoice == 'Rock'){
+        console.log('Computer Wins');
+    }
+    else if (playerChoice == 'Scissors' && computerChoice == 'Paper'){
+        console.log('Player Wins')        ;
+    }
+    else if (playerChoice == 'Scissors' && computerChoice == 'Scissors'){
+        console.log('Tie');
+    }
 }
 
 
