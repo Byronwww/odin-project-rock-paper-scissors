cb557290c2a17ca566bb0be5fb608d9782320610
Byron <byronwmit@gmail.com>
2022-10-31 17:59:58 +0000

    Add image that updates on win/lose conditions

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -31,33 +31,58 @@ function playGame(playerChoice){
     //Player Chooses Rock
     if (playerChoice == 'Rock' && computerChoice == 'Rock'){
         console.log('Tie');
+        changeImage('Tie');
     }
     else if (playerChoice == 'Rock' && computerChoice == 'Paper'){
-        console.log('Computer Wins')        ;
+        console.log('Computer Wins');
+        changeImage('Computer Wins');
     }
     else if (playerChoice == 'Rock' && computerChoice == 'Scissors'){
         console.log('Player Wins');
+        changeImage('Player Wins');
     }
     //Player Chooses Paper
     else if (playerChoice == 'Paper' && computerChoice == 'Rock'){
         console.log('Player Wins');
+        changeImage('Player Wins');
     }
     else if (playerChoice == 'Paper' && computerChoice == 'Paper'){
-        console.log('Tie')        ;
+        console.log('Tie');
+        changeImage('Tie');
     }
     else if (playerChoice == 'Paper' && computerChoice == 'Scissors'){
         console.log('Computer Wins');
+        changeImage('Computer Wins');
     }
     //Player Chooses Scissors
     else if (playerChoice == 'Scissors' && computerChoice == 'Rock'){
         console.log('Computer Wins');
+        changeImage('Computer Wins');
     }
     else if (playerChoice == 'Scissors' && computerChoice == 'Paper'){
-        console.log('Player Wins')        ;
+        console.log('Player Wins');
+        changeImage('Player Wins');
     }
     else if (playerChoice == 'Scissors' && computerChoice == 'Scissors'){
         console.log('Tie');
+        changeImage('Tie');
+    }
+
+    function changeImage(result){   
+        if (result == 'Player Wins'){
+            var image = document.getElementById('image');
+            image.src = "/images/alexKiddOnigiri.jpg";
+        }
+
+        else{
+            var image = document.getElementById('image');
+            image.src = "/images/alexKidd.jpeg";
+        }        
     }
 }
+
+
+
+
 
 
